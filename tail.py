22447c648125c366566d168441117d4876a6c9f3
Alexis Okuwa <wojonstech@gmail.com>
2012-11-09 17:57:35 -0800

    updated the code so that it is able to follow files that are changed during things like log roate

--- tail.py
+++ tail.py
@@ -47,6 +47,7 @@ class Tail(object):
 			self.check_file_validity(tailed_file)
 			self.tailed_files.append(tailed_file)
 		self.callback = sys.stdout.write
+		self.track_name(-1)
 	
 	def follow(self, s=1):
 		''' Do a tail follow. If a callback function is registered it is called with every new line. 
@@ -55,26 +56,31 @@ class Tail(object):
 		Arguments:
 			s - Number of seconds to wait between each iteration; Defaults to 1. '''
 		
-		tailed_points = []
+		self.tailed_points = []
 		self.god_time = time.time()
 		for filename in self.tailed_files:
-			tailed_points.append(open(filename)) #get all the file points
-			tailed_points[len(tailed_points)-1].seek(0, os.SEEK_END) #handle all the seeking
+			self.tailed_points.append(open(filename)) #get all the file points
+			self.tailed_points[len(self.tailed_points)-1].seek(0, os.SEEK_END) #handle all the seeking
 			
 		while True:
 			start_t = time.time() #gotta keep track of how long we been in this
-			for num, file_ in enumerate(tailed_points):
-				line = True #reset line value
-				while line:
-					curr_position = file_.tell() #get current postion
-					line = file_.readline() #read the next line we use readline() and not readlines because we may read a line that is still being written to.
-					if not line: 
-						file_.seek(curr_position) #reset the seek point so we can try from the same point again
-					else:
-						self.exec_callback(line, self.tailed_files[num])
+			for num, file_ in enumerate(self.tailed_points):
+				file_ = self.read_2_EOF(num, file_)
 			time_lap = time.time()-start_t
 			if(time_lap < s):
+				self.check_inode()
 				time.sleep(s - time_lap)
+			
+	def read_2_EOF(self, dex, file_):
+		line = True #reset line value
+		while line:
+			curr_position = file_.tell() #get current postion
+			line = file_.readline() #read the next line we use readline() and not readlines because we may read a line that is still being written to.
+			if not line:
+				file_.seek(curr_position) #reset the seek point so we can try from the same point again
+			else:
+				self.exec_callback(line, self.tailed_files[dex])
+		return file_
 	
 	def exec_callback(self, line, filename):		
 		if self.callback_mode == 0:
@@ -86,6 +92,21 @@ class Tail(object):
 		''' Overrides default callback function to provided function. '''
 		self.callback = func
 		self.callback_mode = mode
+		
+	def track_name(self, timer):
+		self.follow_timer = {'wait' : timer, 'last' : time.time()}
+		self.inode = list()
+		for filename in self.tailed_files: #loop though all the files we want to track
+			self.inode.append(os.stat(filename).st_ino) #get the inode of the file
+	
+	def check_inode(self):
+		if self.follow_timer['wait'] > 0 and time.time()-self.follow_timer['last'] >= self.follow_timer['wait']: #check to see if we have this tracking on
+			for dex, filename in enumerate(self.tailed_files):
+				if os.stat(filename).st_ino != self.inode[dex]: #check if the inode has changed
+					self.read_2_EOF(dex, self.tailed_points[dex]) #read the current but old file to EOF
+					self.tailed_points[dex] = open(filename) # reopen the file pointer to the replacment file
+					self.tailed_points[dex].seek(0, os.SEEK_SET)#change the location on the file pointer to be the start of the file
+					self.inode[dex] = os.stat(filename).st_ino #write the new inode
 
 	def check_file_validity(self, file_):
 		''' Check whether the a given file exists, readable and is a file '''
