3389ad9c701bd0dea8cac47489b54734e9d1d035
Alexis Okuwa <wojonstech@gmail.com>
2014-09-26 05:24:06 -0700

    removed all the arrays going on and mereged it into a single dict and added a lot of little helper functions

--- tail.py
+++ tail.py
@@ -29,10 +29,8 @@ import time
 
 class Tail(object):
     ''' Represents a tail command. '''
-    tailed_files  = list()
-    tailed_points = list()
-    callbacks     = dict()
-    inode         = list()
+    follow_filename = False
+    follows = dict()
     
     def __init__(self):
         ''' Initiate a Tail instance.
@@ -41,19 +39,19 @@ class Tail(object):
             Arguments:
                 tailed_file - File to be followed. '''
     
-    def tail(self, tailed_files, callback=None):
-        if type(tailed_files).__name__ == 'str': #we allow strings we will just handle the converstion
-            tailed_files = [tailed_files]
+    def tail(self, files, callback=None):
+        if type(files).__name__ == 'str': #we allow strings we will just handle the converstion
+            files = [files]
         
-        for tailed_file in tailed_files: #make sure we have good files all around
+        for filename in files: #make sure we have good files all around
             try:
-                self.check_file_validity(tailed_file)
+                self.check_file_validity(filename)
             except TailError as e:
                 sys.stderr.write(str(e))
-                
-            if tailed_file not in self.tailed_files: #make sure we dont have the file already
-                self.tailed_files.append(tailed_file)
-                self.callbacks[tailed_file] = callback #if callback == None else sys.stdout.write
+            
+            if filename not in self.follows: #make sure we dont have the file already
+                self.new_follow(filename)
+                self.set_callback(filename, callback)
         
         #self.callback = sys.stdout.write
     
@@ -73,26 +71,27 @@ class Tail(object):
             loop_max - the max number of times the loop should run over
         '''
         
-        for filename in self.tailed_files:
-            if len(self.tailed_points)-1 < self.tailed_files.index(filename):
-                self.tailed_points.append(self.open_file(filename)) #get all the file points
+        for filename in self.follows:
+            
+            if self.get_filename_fd(filename) is None:
+                self.open_file(filename) #get all the file points
                 
-                if self.tailed_points[len(self.tailed_points)-1] != None:
-                    self.tailed_points[len(self.tailed_points)-1].seek(0, os.SEEK_END) #handle all the seeking
+                if self.get_filename_fd(filename) != None:
+                    self.follows[filename]['fd'].seek(0, os.SEEK_END) #handle all the seeking
         
         follow_t = time.time()
         
         while loop_max != 0 and (max_time == None or time.time() <= max_time+follow_t):
             start_t  = time.time() #gotta keep track of how long we been in this
-                
-            for num, file_ in enumerate(self.tailed_points):
-                if file_ != None:
-                    file_ = self.read_2_EOF(num, file_)
+            
+            for filename, file_ in self.follows.iteritems():
+                if file_['fd'] != None:
+                    file_ = self.read_2_EOF(filename, file_['fd'])
                 else:
-                    self.tailed_points[num] = self.open_file(self.tailed_files[num])
+                    self.open_file(filename)
                 
                 if self.follow_filename == True: #check to see if inode has changed
-                    self.check_inode(num, self.tailed_files[num])
+                    self.check_inode(filename)
                     
             if loop_max != True:
                 loop_max -= 1
@@ -102,7 +101,7 @@ class Tail(object):
             time_lap = time.time()-start_t
             if(time_lap < s):  time.sleep(s - time_lap)
             
-    def read_2_EOF(self, dex, file_):
+    def read_2_EOF(self, filename, file_):
         if file_ == None: return None
             
         line = True #reset line value
@@ -112,12 +111,12 @@ class Tail(object):
             if not line:
                 file_.seek(curr_position) #reset the seek point so we can try from the same point again
             else:
-                self.exec_callback(line, self.tailed_files[dex])
+                self.exec_callback(line, filename)
         return file_
     
     def exec_callback(self, line, filename):
-        if filename in self.callabcks:
-            self.callbacks[filename]({'line':line, 'filename':filename})
+        if filename in self.follows and self.follows[filename].get('callback') is not None:
+            self.follows[filename]['callback']({'line':line, 'filename':filename})
         else:
             print ({'line':line, 'filename':filename})
         
@@ -127,11 +126,17 @@ class Tail(object):
         except OSError as e:
             return None
             
+    def get_filename_inode(self, filename):
+        if filename in self.follows: return self.follows[filename].get('inode')
+            
     def get_file_num(self, filename):
         try:
             return self.tailed_files.index(filename)
         except:
             return None
+            
+    def set_callback(self, filename, callback):
+        self.follows[filename]['callback'] = callback
             
     def open_file(self, filename):
         try:
@@ -139,26 +144,30 @@ class Tail(object):
             if not os.access(filename, os.R_OK): return None
             if os.path.isdir(filename):  return None
                 
-            file_ = open(filename)
+            self.follows[filename]['fd'] = open(filename)
+            self.follows[filename]['inode'] = self.get_inode(filename)
             
-            if self.get_file_num(filename) in self.inode:
-                self.inode[self.get_file_num(filename)] = self.get_inode(filename)
-            else:
-                self.inode.append(self.get_inode(filename))
-            
-            return file_
-        
         except (IOError, OSError) as e:
-            return None
-            
-    def check_inode(self, num, filename):
+            pass
+    
+    def get_filename_fd(self, filename):
+        if filename in self.follows: return self.follows[filename].get('fd')
+        
+    def check_inode(self, filename):
         try:
-            if self.get_inode(filename) != self.inode[num]: #check if the inode has changed
-                self.read_2_EOF(num, self.tailed_points[num]) #read the current but old file to EOF
-                self.tailed_points[num] = self.open_file(filename) # reopen the file pointer to the replacment file
+            if self.get_inode(filename) != self.get_filename_inode(filename): #check if the inode has changed
+                self.read_2_EOF(filename, self.get_filename_fd(filename)) #read the current but old file to EOF
+                self.open_file(filename) # reopen the file pointer to the replacment file
                 
         except OSError as e:
             pass
+            
+    def new_follow(self, filename, overwrite=False):
+        if filename not in self.follows or overwrite is True:
+            self.follows[filename] = {'fd': None, 'callback': None, 'inode': None}
+            return True
+            
+        return True
 
     def check_file_validity(self, file_):
         ''' Check whether the a given file exists, readable and is a file '''
@@ -174,4 +183,3 @@ class TailError(Exception):
         self.message = msg
     def __str__(self):
         return self.message
-
